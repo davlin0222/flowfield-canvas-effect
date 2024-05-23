06ad0ffd7e58f642914144e69b6957d123e59358
David Lindström <davlin@creatorise.com>
2024-05-23 22:54:13 +0200

    Add grid of lines

--- dist/src/render-canvas-effect.js
+++ dist/src/render-canvas-effect.js
@@ -1,7 +1,16 @@
 export function renderCanvasEffect(canvasContext) {
+    const width = canvasContext.canvas.width;
+    const height = canvasContext.canvas.height;
     canvasContext.strokeStyle = "gray";
     canvasContext.lineWidth = 3;
-    drawLine(5, 5, 100);
+    drawGridOfLines(100, 50);
+    function drawGridOfLines(gridCellSize, lineLength) {
+        for (let y = 0; y < height; y += gridCellSize) {
+            for (let x = 0; x < width; x += gridCellSize) {
+                drawLine(x, y, lineLength);
+            }
+        }
+    }
     function drawLine(x, y, length) {
         canvasContext.beginPath();
         canvasContext.moveTo(x, y);
--- src/render-canvas-effect.ts
+++ src/render-canvas-effect.ts
@@ -1,8 +1,17 @@
 export function renderCanvasEffect(canvasContext: CanvasRenderingContext2D) {
+	const width = canvasContext.canvas.width;
+	const height = canvasContext.canvas.height;
+
 	canvasContext.strokeStyle = "gray";
 	canvasContext.lineWidth = 3;
-	drawLine(5, 5, 100);
-
+	drawGridOfLines(100, 50);
+	function drawGridOfLines(gridCellSize: number, lineLength: number) {
+		for (let y = 0; y < height; y += gridCellSize) {
+			for (let x = 0; x < width; x += gridCellSize) {
+				drawLine(x, y, lineLength);
+			}
+		}
+	}
 	function drawLine(x: number, y: number, length: number) {
 		canvasContext.beginPath();
 		canvasContext.moveTo(x, y);
